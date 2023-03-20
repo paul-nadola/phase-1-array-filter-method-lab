9140649a05317cb1f36a0feec9ec6ab4f753b50d
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-20 23:45:56 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+console.log(findMatching(drivers, 'Bobby'))
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
